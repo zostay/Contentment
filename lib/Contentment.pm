68a972ef85d53351432afd491b7a903218c77645
Sterling Hanenkamp <hanenkamp@cpan.org>
2005-11-03 20:22:58 +0000

    Contentment 0.11.21 Beta:
        * Removed the unimplemented plugins directory.

--- lib/Contentment.pm
+++ lib/Contentment.pm
@@ -3,7 +3,7 @@ package Contentment;
 use strict;
 use warnings;
 
-our $VERSION = 0.011_020;
+our $VERSION = 0.011_021;
 
 use Carp;
 use Contentment::Hooks;
--- plugins/unimplemented/security-dbi/docs/security/logout.pl
+++
@@ -1,4 +0,0 @@
-<%init>
-delete $context->session->{current_user};
-return $context->action_result('Success');
-</%init>
