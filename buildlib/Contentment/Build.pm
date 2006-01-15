b378fd6ff292cfb03b3f2c5772bf80eca9f49c2b
Sterling Hanenkamp <hanenkamp@cpan.org>
2006-01-15 04:24:06 +0000

    * Fixed what I hope to be the last bug in upload target of the Build script.

--- buildlib/Contentment/Build.pm
+++ buildlib/Contentment/Build.pm
@@ -568,6 +568,7 @@ sub ACTION_upload_release_to_PAUSE {
     _STERLING_ONLY('Sterling will be ticked off if someone other than him uploads a Contentment release to CPAN. Stop it.');
 
     my $version = _get_version();
+    $version =~ s/_//; # ./Build dist doesn't do that
 
     require YAML;
 
@@ -589,7 +590,7 @@ sub ACTION_upload_release_to_PAUSE {
     my $ftp = Net::FTP->new('pause.perl.org')
         or die "Cannot connect to pause.perl.org: $@";
     
-    print "Logging in as $upload->{username}.\n";
+    print "Logging in as anonymous : hanenkamp\@cpan.org.\n";
     $ftp->login('anonymous', 'hanenkamp@cpan.org')
         or die "Cannot login as anonymous on pause.perl.org: ",$ftp->message;
 
